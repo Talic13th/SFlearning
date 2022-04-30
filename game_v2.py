41306f66e7f96aaff5b8194e29302d7f424fc2d3
13th <talic13th@gmail.com>
2022-04-30 12:56:19 +0800

    init

---
+++ game_v2.py
@@ -0,0 +1,10 @@
+import numpy as np
+
+def random_predict(number:int=1) -> int:
+    """Рандомно угадываем число
+
+    Args:
+        number (int, optional): Загаданное число. Defaults to 1.
+
+    Returns:
+        int: Число попыток"""
